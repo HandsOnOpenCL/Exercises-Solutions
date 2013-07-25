934f183804246b8ff62f88fa328557c603ae453e
Tom Deakin <thomasdeakin@gmail.com>
2013-07-25 15:01:19 +0100

    Serial pi code, copied in from reference code
    Closes #8.

---
+++ Exercises/Exercise08/C/pi.c
@@ -0,0 +1,41 @@
+/*
+
+This program will numerically compute the integral of
+
+                  4/(1+x*x) 
+				  
+from 0 to 1.  The value of this integral is pi -- which 
+is great since it gives us an easy way to check the answer.
+
+The is the original sequential program.  It uses the timer
+from the OpenMP runtime library
+
+History: Written by Tim Mattson, 11/99.
+
+*/
+
+#include <stdio.h>
+static long num_steps = 100000000;
+double step;
+extern double wtime();   // returns time since some fixed past point (wtime.c)
+
+
+int main ()
+{
+    int i;
+    double x, pi, sum = 0.0;
+    double start_time, run_time;
+
+    step = 1.0/(double) num_steps;
+
+    start_time =wtime();
+
+    for (i=1;i<= num_steps; i++){
+        x = (i-0.5)*step;
+        sum = sum + 4.0/(1.0+x*x);
+    }
+
+    pi = step * sum;
+    run_time = wtime() - start_time;
+    printf("\n pi with %ld steps is %lf in %lf seconds\n", num_steps, pi, run_time);
+}	  
