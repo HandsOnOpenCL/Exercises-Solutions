c35c93eabe55398ea3cc113e0a747c5560fe4546
Tom Deakin <thomasdeakin@gmail.com>
2013-07-25 23:52:32 +0100

    Copy in matrix code from Ex6
    The matmul.c driver needs to be expended for the other kernels still

---
+++ Solutions/Exercise07/C/matmul.h
@@ -0,0 +1,53 @@
+//------------------------------------------------------------------------------
+//
+//  Include fle for the Matrix Multiply test harness
+//
+//  HISTORY: Written by Tim Mattson, August 2010
+//           Modified by Simon McIntosh-Smith, September 2011
+//           Modified by Tom Deakin and Simon McIntosh-Smith, October 2012
+//           Ported to C by Tom Deakin, July 2013
+//
+//------------------------------------------------------------------------------
+
+#ifndef __MULT_HDR
+#define __MULT_HDR
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#ifdef APPLE
+#include <OpenCL/opencl.h>
+#include <unistd.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#include "matrix_lib.h"
+
+//pick up device type from compiler command line or from 
+//the default type
+#ifndef DEVICE
+#define DEVICE CL_DEVICE_TYPE_GPU
+#endif
+
+//------------------------------------------------------------------------------
+//  functions from ../Common
+//------------------------------------------------------------------------------
+extern int    output_device_info(cl_device_id );
+extern int    err_code(cl_int);
+extern double wtime();   // returns time since some fixed past point (wtime.c)
+
+//------------------------------------------------------------------------------
+//  Constants
+//------------------------------------------------------------------------------
+#define ORDER    1024    // Order of the square matrices A, B, and C
+#define AVAL     3.0     // A elements are constant and equal to AVAL
+#define BVAL     5.0     // B elements are constant and equal to BVAL
+#define TOL      (0.001) // tolerance used in floating point comparisons
+#define DIM      2       // Max dim for NDRange
+#define COUNT    1       // number of times to do each multiplication
+#define SUCCESS  1
+#define FAILURE  0
+
+#endif
