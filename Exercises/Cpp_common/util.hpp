0e8182edb98f397bde36b50b87f12b10e9bb82f1
Tom Deakin <thomasdeakin@gmail.com>
2013-08-22 23:08:56 +0100

    Example vector add - ex3

---
+++ Exercises/Cpp_common/util.hpp
@@ -0,0 +1,202 @@
+#ifndef __UTIL_HDR
+#define __UTIL_HDR
+
+#if defined(_WIN32)
+#include <windows.h>
+typedef unsigned __int64 uint64_t;
+#elif defined(__APPLE__) || defined(__MACOSX)
+#include <sys/time.h>
+#else
+#include <stdint.h>
+#include <unistd.h>
+#endif
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <cstdlib>
+
+namespace util {
+
+inline std::string loadProgram(std::string input)
+{
+    std::ifstream stream(input.c_str());
+    if (!stream.is_open()) {
+        std::cout << "Cannot open file: " << input << std::endl;
+        exit(1);
+    }
+
+     return std::string(
+        std::istreambuf_iterator<char>(stream),
+        (std::istreambuf_iterator<char>()));
+}
+
+#if 1
+class Timer
+{
+private:
+#if defined(_WIN32)
+    LARGE_INTEGER frequency_;
+    DWORD         startTick_;
+    LONGLONG      prevElapsedTime_;
+    LARGE_INTEGER startTime_;
+#elif defined(__APPLE__) || defined(__MACOSX)
+    struct timeval startTime_;
+#else
+    struct timespec startTime_;
+#endif //_WIN32
+
+    template <typename T>
+    T _max(T a,T b)
+    {
+        return (a > b ? a : b);
+    }
+
+    uint64_t getTime(unsigned long long scale)
+    {
+        uint64_t ticks;
+#if defined(_WIN32)
+        LARGE_INTEGER currentTime;
+        QueryPerformanceCounter(&currentTime);
+        LONGLONG elapsedTime = currentTime.QuadPart - startTime_.QuadPart;
+
+        // Compute the number of millisecond ticks elapsed.
+        unsigned long msecTicks =
+            (unsigned long)(1000 * elapsedTime / frequency_.QuadPart);
+        // Check for unexpected leaps in the Win32 performance counter.
+        // (This is caused by unexpected data across the PCI to ISA
+        // bridge, aka south bridge.  See Microsoft KB274323.)
+        unsigned long elapsedTicks = GetTickCount() - startTick_;
+
+        signed long msecOff = (signed long)(msecTicks - elapsedTicks);
+        if (msecOff < -100 || msecOff > 100) {
+                // Adjust the starting time forwards.
+                LONGLONG msecAdjustment =
+                    _max(msecOff *
+                        frequency_.QuadPart / 1000, elapsedTime -
+                        prevElapsedTime_);
+                startTime_.QuadPart += msecAdjustment;
+                elapsedTime -= msecAdjustment;
+        }
+        // Store the current elapsed time for adjustments next time.
+        prevElapsedTime_ = elapsedTime;
+
+        ticks = (uint64_t)(scale*elapsedTime / frequency_.QuadPart);
+#elif defined(__APPLE__) || defined(__MACOSX)
+        // WARNING: THIS IS PROBABLY BROKEN
+        struct timeval tv;
+        gettimeofday(&tv, 0);
+        ticks = (uint64_t) (tv.tv_sec - startTime_.tv_sec) * scale
+                + (uint64_t) (tv.tv_usec - startTime_.tv_sec);
+#else
+        struct timespec tp;
+        ::clock_gettime(CLOCK_MONOTONIC, &tp);
+        // check for overflow
+        if ((tp.tv_nsec - startTime_.tv_nsec) < 0)
+        {
+            // Remove a second from the second field and add it to the
+            // nanoseconds field to prevent overflow.
+            // Then scale
+            ticks = (uint64_t) (tp.tv_sec - startTime_.tv_sec - 1) * scale
+                    + (uint64_t) ((1000ULL * 1000ULL * 1000ULL) + tp.tv_nsec - startTime_.tv_nsec)
+                                  * scale / (1000ULL * 1000ULL * 1000ULL);
+        }
+        else
+        {
+            ticks = (uint64_t) (tp.tv_sec - startTime_.tv_sec) * scale
+                + (uint64_t) (tp.tv_nsec - startTime_.tv_nsec) * scale / (1000ULL * 1000ULL * 1000ULL);
+        }
+#endif //_WIN32
+
+        return ticks;
+    }
+
+public:
+    //! Constructor
+    Timer()
+    {
+#if defined(_WIN32)
+        QueryPerformanceFrequency(&frequency_);
+#endif
+        reset();
+    }
+
+    //! Destructor
+    ~Timer()
+    {
+    }
+
+    /*!
+     * \brief Resets timer such that in essence the elapsed time is zero
+     * from this point.
+     */
+    void reset()
+    {
+#if defined(_WIN32)
+        QueryPerformanceCounter(&startTime_);
+        startTick_ = GetTickCount();
+        prevElapsedTime_ = 0;
+#elif defined(__APPLE__) || defined(__MACOSX)
+        gettimeofday(&startTime_, 0);
+#else
+        ::clock_gettime(CLOCK_MONOTONIC, &startTime_);
+#endif
+    }
+
+    /*!
+     * \brief Calculates the time since the last reset.
+     * \returns The time in milli seconds since the last reset.
+     */
+    uint64_t getTimeMilliseconds(void)
+    {
+        return getTime(1000ULL);
+    }
+
+    /*!
+     * \brief Calculates the time since the last reset.
+     * \returns The time in nano seconds since the last reset.
+     */
+    uint64_t getTimeNanoseconds(void)
+    {
+        return getTime(1000ULL * 1000ULL * 1000ULL);
+    }
+
+    /*!
+     * \brief Calculates the time since the last reset.
+     * \returns The time in micro seconds since the last reset.
+     */
+    uint64_t getTimeMicroseconds(void)
+    {
+        return getTime(1000ULL * 1000ULL);
+    }
+
+    /*!
+     * \brief Calculates the tick rate for millisecond counter.
+     */
+    float getMillisecondsTickRate(void)
+    {
+        return 1000.f;
+    }
+
+    /*!
+     * \brief Calculates the tick rate for nanosecond counter.
+     */
+    float getNanosecondsTickRate(void)
+    {
+        return (float) (1000ULL * 1000ULL * 1000ULL);
+    }
+
+    /*!
+     * \brief Calculates the tick rate for microsecond counter.
+     */
+    float getMicrosecondsTickRate(void)
+    {
+        return (float) (1000ULL * 1000ULL);
+    }
+};
+#endif
+
+} // namespace util
+
+#endif // __UTIL_HDR
---
+++ Exercises/Exercise03/Cpp/vadd.cpp
@@ -0,0 +1,136 @@
+//------------------------------------------------------------------------------
+//
+// Name:       vadd_cpp.cpp
+// 
+// Purpose:    Elementwise addition of two vectors (c = a + b)
+//
+//                   c = a + b
+//
+// HISTORY:    Written by Tim Mattson, June 2011
+//             Ported to C++ Wrapper API by Benedict Gaster, September 2011
+//             Updated to C++ Wrapper API v1.2 by Tom Deakin and Simon McIntosh-Smith, October 2012
+//             Updated to C++ Wrapper v1.2.6 by Tom Deakin, August 2013
+//             
+//------------------------------------------------------------------------------
+
+#define __CL_ENABLE_EXCEPTIONS
+
+#include "cl.hpp"
+
+#include "util.hpp" // utility library
+
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+#include <iostream>
+#include <fstream>
+
+// pick up device type from compiler command line or from the default type
+#ifndef DEVICE
+#define DEVICE CL_DEVICE_TYPE_DEFAULT
+#endif
+
+
+//------------------------------------------------------------------------------
+
+#define TOL    (0.001)   // tolerance used in floating point comparisons
+#define LENGTH (1024)    // length of vectors a, b, and c
+
+int main(void)
+{
+    std::vector<float> h_a(LENGTH);                // a vector 
+    std::vector<float> h_b(LENGTH);                // b vector 	
+    std::vector<float> h_c (LENGTH, 0xdeadbeef);    // c = a + b, from compute device
+
+    cl::Buffer d_a;                        // device memory used for the input  a vector
+    cl::Buffer d_b;                        // device memory used for the input  b vector
+    cl::Buffer d_c;                       // device memory used for the output c vector
+
+    // Fill vectors a and b with random float values
+    int count = LENGTH;
+    for(int i = 0; i < count; i++)
+    {
+        h_a[i]  = rand() / (float)RAND_MAX;
+        h_b[i]  = rand() / (float)RAND_MAX;
+    }
+
+    try 
+    {
+    	// Create a context
+        cl::Context context(DEVICE);
+
+        // Load in kernel source, creating a program object for the context
+
+        cl::Program program(context, util::loadProgram("vadd.cl"), true);
+
+        // Get the command queue
+        cl::CommandQueue queue(context);
+
+        // Create the kernel functor
+ 
+        auto vadd = cl::make_kernel<cl::Buffer, cl::Buffer, cl::Buffer, int>(program, "vadd");
+
+        d_a   = cl::Buffer(context, begin(h_a), end(h_a), true);
+        d_b   = cl::Buffer(context, begin(h_b), end(h_b), true);
+
+        d_c  = cl::Buffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * LENGTH);
+
+        util::Timer timer;
+
+        vadd(
+            cl::EnqueueArgs(
+                queue,
+                cl::NDRange(count)), 
+            d_a,
+            d_b,
+            d_c,
+            count);
+
+        double rtime = static_cast<double>(timer.getTimeMilliseconds()) / 1000.0;
+        printf("\nThe kernels ran in %lf seconds\n", rtime);
+
+        queue.enqueueReadBuffer(
+            d_c, 
+            CL_TRUE,
+            0, sizeof(float) * LENGTH,
+            &h_c[0]);
+
+        // Test the results
+        int correct = 0;
+        float tmp;
+        for(int i = 0; i < count; i++) {
+            tmp = h_a[i] + h_b[i]; // expected value for d_c[i]
+            tmp -= h_c[i];                      // compute errors
+            if(tmp*tmp < TOL*TOL) {      // correct if square deviation is less 
+                correct++;                         //  than tolerance squared
+            }
+            else {
+
+                printf(
+                    " tmp %f h_a %f h_b %f  h_c %f \n",
+                    tmp, 
+                    h_a[i], 
+                    h_b[i], 
+                    h_c[i]);
+            }
+        }
+
+        // summarize results
+        printf(
+            "vector add to find C = A+B:  %d out of %d results were correct.\n", 
+            correct, 
+            count);
+    }
+    catch (cl::Error err) {
+        std::cout << "Exception\n";
+        std::cerr 
+            << "ERROR: "
+            << err.what()
+            << "("
+            << err.err()
+           << ")"
+           << std::endl;
+    }
+}
