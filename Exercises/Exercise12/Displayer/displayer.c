2bd1d0c7df3ff4c89d22eff8adddc2b5a2730b75
Tom Deakin <thomasdeakin@gmail.com>
2013-08-24 10:48:28 +0100

    include GL headers from correct places on Apple

--- Exercises/Exercise12/Displayer/displayer.c
+++ Exercises/Exercise12/Displayer/displayer.c
@@ -1,6 +1,11 @@
 
-#include <GL/glut.h>
-#include <GL/gl.h>
+#ifdef __APPLE__
+    #include <OpenGL/gl.h>
+    #include <GLUT/glut.h>
+#else
+    #include <GL/gl.h>
+    #include <GL/glut.h>
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
