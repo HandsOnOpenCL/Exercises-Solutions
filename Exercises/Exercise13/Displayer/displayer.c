01a0c53d505cf2da5d6bf0048e270f8ff2553265
Tom Deakin <thomasdeakin@gmail.com>
2013-09-03 15:26:11 +0100

    pan implemented, zoom a bit weird

--- Exercises/Exercise13/Displayer/displayer.c
+++ Exercises/Exercise13/Displayer/displayer.c
@@ -14,6 +14,16 @@
 
 GLuint texture;
 
+float tex_points[] = {0.0, 1.0,
+                      0.0, 0.0,
+                      1.0, 0.0,
+                      1.0, 1.0};
+
+float quad_points[] = {-1.0, -1.0,
+                       -1.0,  1.0,
+                        1.0,  1.0,
+                        1.0, -1.0};
+
 void display(void)
 {
     // Clear the colour buffer for the new display
@@ -26,14 +36,86 @@ void display(void)
     glBindTexture(GL_TEXTURE_2D, texture);
     // Draw a rectangle the size of the display
     glBegin(GL_QUADS);
-        glTexCoord2f(0.0,1.0); glVertex2f(-1.0,-1.0);
-        glTexCoord2f(0.0,0.0); glVertex2f(-1.0,1.0);
-        glTexCoord2f(1.0,0.0); glVertex2f(1.0,1.0);
-        glTexCoord2f(1.0,1.0); glVertex2f(1.0,-1.0);
+        glTexCoord2f(tex_points[0],tex_points[1]); glVertex2f(quad_points[0],quad_points[1]);
+        glTexCoord2f(tex_points[2],tex_points[3]); glVertex2f(quad_points[2],quad_points[3]);
+        glTexCoord2f(tex_points[4],tex_points[5]); glVertex2f(quad_points[4],quad_points[5]);
+        glTexCoord2f(tex_points[6],tex_points[7]); glVertex2f(quad_points[6],quad_points[7]);
     glEnd();
     // Display!
     glFlush();
     glDisable(GL_TEXTURE_2D);
+}
+
+void press(unsigned char key, int x, int y)
+{
+#define PAN 0.01
+#define ZOOM 2.0
+    switch (key)
+    {
+        case 'w':
+            tex_points[1] += PAN;
+            tex_points[3] += PAN;
+            tex_points[5] += PAN;
+            tex_points[7] += PAN;
+            break;
+        case 'a':
+            tex_points[0] += PAN;
+            tex_points[2] += PAN;
+            tex_points[4] += PAN;
+            tex_points[6] += PAN;
+            break;
+        case 's':
+            tex_points[1] -= PAN;
+            tex_points[3] -= PAN;
+            tex_points[5] -= PAN;
+            tex_points[7] -= PAN;
+            break;
+        case 'd':
+            tex_points[0] -= PAN;
+            tex_points[2] -= PAN;
+            tex_points[4] -= PAN;
+            tex_points[6] -= PAN;
+            break;
+        case 'i':
+            quad_points[0] *= ZOOM;
+            quad_points[1] *= ZOOM;
+            quad_points[2] *= ZOOM;
+            quad_points[3] *= ZOOM;
+            quad_points[4] *= ZOOM;
+            quad_points[5] *= ZOOM;
+            quad_points[6] *= ZOOM;
+            quad_points[7] *= ZOOM;
+            break;
+        case 'o':
+            quad_points[0] /= ZOOM;
+            quad_points[1] /= ZOOM;
+            quad_points[2] /= ZOOM;
+            quad_points[3] /= ZOOM;
+            quad_points[4] /= ZOOM;
+            quad_points[5] /= ZOOM;
+            quad_points[6] /= ZOOM;
+            quad_points[7] /= ZOOM;
+            break;
+        case 'r':
+            tex_points[0] = 0.0;
+            tex_points[1] = 1.0;
+            tex_points[2] = 0.0;
+            tex_points[3] = 0.0;
+            tex_points[4] = 1.0;
+            tex_points[5] = 0.0;
+            tex_points[6] = 1.0;
+            tex_points[7] = 1.0;
+            quad_points[0] = -1.0;
+            quad_points[1] = -1.0;
+            quad_points[2] = -1.0;
+            quad_points[3] = 1.0;
+            quad_points[4] = 1.0;
+            quad_points[5] = 1.0;
+            quad_points[6] = 1.0;
+            quad_points[7] = -1.0;
+            break;
+    }
+    glutPostRedisplay();
 }
 
 int main(int argc, char **argv)
@@ -91,11 +173,15 @@ int main(int argc, char **argv)
     // Specify what to do when we stretch/shrink the texture to fit on the screen
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     // Create the texture itself
     glTexImage2D(GL_TEXTURE_2D, 0, 4, nx, ny, 0, GL_RED, GL_UNSIGNED_BYTE, &board[0]);
 
     // Set display call back
     glutDisplayFunc(display);
+    // Set keyboard call back
+    glutKeyboardFunc(press);
 
     // Show the screen
     glutMainLoop();
