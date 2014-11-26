e33f1c0198aa804f2ae2c27ea7e9a1e4cf3a8b45
Ben Elgar <me@benelgar.com>
2014-11-26 21:42:50 +0000

    Update Matrix Multiply solution for square assumption

--- Solutions/Exercise06/Python/matmul.py
+++ Solutions/Exercise06/Python/matmul.py
@@ -9,10 +9,11 @@
 # set as a constant, ORDER (see definitions.py). This is so
 # we can make a quick test of the multiplication result.
 #
-# History:   C++ version written by Tim Mattson, August 2010 
+# History:   C++ version written by Tim Mattson, August 2010
 #            Modified by Simon McIntosh-Smith, September 2011
 #            Modified by Tom Deakin and Simon McIntosh-Smith, October 2012
 #            Ported to Python by Tom Deakin, July 2013
+#            Modified to assume square matrices by Ben Elgar, November 2014
 #
 
 from helper import *
@@ -24,48 +25,44 @@ from time import time
 
 C_elem_KernelSource = '''
 __kernel void mmul(
-	const int Mdim,
-	const int Ndim,
-	const int Pdim,
-	__global float* A,
-	__global float* B,
-	__global float* C)
+    const int N,
+    __global float* A,
+    __global float* B,
+    __global float* C)
 {
-	int k;
-	int i = get_global_id(0);
-	int j = get_global_id(1);
-	float tmp;
-	if ((i < Ndim) && (j < Mdim))
-	{
-		tmp = 0.0f;
-		for (k = 0; k < Pdim; k++)
-			tmp += A[i*Ndim+k] * B[k*Pdim+j];
-		C[i*Ndim+j] = tmp;
-	}
+    int k;
+    int i = get_global_id(0);
+    int j = get_global_id(1);
+    float tmp = 0;
+    if ((i < N) && (j < N))
+    {
+        tmp = 0.0f;
+        for (k=0; k<N; k++)
+        {
+            tmp += A[i*N + k] * B[k*N + j];
+        }
+        C[i*N + j] = tmp;
+    }
 }
 '''
 
-# A[N][P], B[P][M], C[N][M]
-Ndim = ORDER;
-Pdim = ORDER;
-Mdim = ORDER;
+# A[N][N], B[N][N], C[N][N]
+N = ORDER;
 
 # Number of elements in the matrix
-sizeA = Ndim * Pdim
-sizeB = Pdim * Mdim
-sizeC = Ndim * Mdim
+size = N * N
 
 
 # A matrix
-h_A = numpy.empty(sizeA).astype(numpy.float32)
+h_A = numpy.empty(size).astype(numpy.float32)
 h_A.fill(AVAL)
 
 # B matrix
-h_B = numpy.empty(sizeB).astype(numpy.float32)
+h_B = numpy.empty(size).astype(numpy.float32)
 h_B.fill(BVAL)
 
 # C matrix
-h_C = numpy.empty(sizeC).astype(numpy.float32)
+h_C = numpy.empty(size).astype(numpy.float32)
 
 print "\n===== Sequential, matrix mult (dot prod), order", ORDER, "on host CPU ======\n"
 
@@ -74,10 +71,10 @@ for i in range(COUNT):
     start_time = time()
 
     print "Skipping as this takes a long time to run!"
-    #seq_mat_mul_sdot(Mdim, Ndim, Pdim, h_A, h_B, h_C)
+    #seq_mat_mul_sdot(N, h_A, h_B, h_C)
 
     run_time = time() - start_time
-    #results(Mdim, Ndim, Pdim, h_C, run_time)
+    #results(N, h_C, run_time)
 
 
 # Set up OpenCL
@@ -85,11 +82,11 @@ context = cl.create_some_context()
 queue = cl.CommandQueue(context)
 
 # Reset host buffers - just to play it safe
-h_A = numpy.empty(sizeA).astype(numpy.float32)
+h_A = numpy.empty(size).astype(numpy.float32)
 h_A.fill(AVAL)
-h_B = numpy.empty(sizeB).astype(numpy.float32)
+h_B = numpy.empty(size).astype(numpy.float32)
 h_B.fill(BVAL)
-h_C = numpy.empty(sizeC).astype(numpy.float32)
+h_C = numpy.empty(size).astype(numpy.float32)
 
 # Create OpenCL buffers
 d_a = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=h_A)
@@ -98,22 +95,22 @@ d_c = cl.Buffer(context, cl.mem_flags.WRITE_ONLY, h_C.nbytes)
 
 program = cl.Program(context, C_elem_KernelSource).build()
 mmul = program.mmul
-mmul.set_scalar_arg_dtypes([numpy.int32, numpy.int32, numpy.int32, None, None, None])
+mmul.set_scalar_arg_dtypes([numpy.int32, None, None, None])
 
-print "\n===== OpenCL, matrix mult, C(i,j) per work item, order", Ndim, "======\n"
+print "\n===== OpenCL, matrix mult, C(i,j) per work item, order", N, "======\n"
 
 # Do the multiplication COUNT times
 for i in range(COUNT):
     h_C.fill(0.0)
     start_time = time()
 
-    globalrange = (Ndim, Mdim)
+    globalrange = (N, N)
     localrange = None
 
-    mmul(queue, globalrange, localrange, Mdim, Ndim, Pdim, d_a, d_b, d_c)
+    mmul(queue, globalrange, localrange, N, d_a, d_b, d_c)
     queue.finish()
 
     run_time = time() - start_time
 
     cl.enqueue_copy(queue, h_C, d_c)
-    results(Mdim, Ndim, Pdim, h_C, run_time)
+    results(N, h_C, run_time)
